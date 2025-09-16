d87f184449c82c80c39dcc82bcee23171c351f46
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-09-16 12:25:20 +0300

    Not Found Page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom"
 
 function NotFoundPage() {
+
+  
   return (
-    <div>NotFoundPage</div>
+    <section className="flex flex-col items-center justify-center h-screen gap-6">
+      <h1 className="text-white font-bold text-4xl">Not Found Page </h1>
+      <Link className="px-4 py-2 border rounded-xl hover:bg-[#1d0136]"  to="/">Home Page</Link>
+    </section>
   )
 }
 
